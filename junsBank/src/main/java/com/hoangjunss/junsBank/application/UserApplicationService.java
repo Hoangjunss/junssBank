1f2185c6ebb5cec241b4d6ad4041999c0d7ed60e
Hoangjunss <109050903+Hoangjunss@users.noreply.github.com>
2024-11-12 19:51:04 +0700

    add login

--- junsBank/src/main/java/com/hoangjunss/junsBank/application/UserApplicationService.java
+++ junsBank/src/main/java/com/hoangjunss/junsBank/application/UserApplicationService.java
@@ -1,28 +1,47 @@
 package com.hoangjunss.junsBank.application;
 
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.hoangjunss.junsBank.config.JwtTokenUtil;
+import com.hoangjunss.junsBank.contants.RedisContants;
+import com.hoangjunss.junsBank.dto.user.AccountDTO;
+import com.hoangjunss.junsBank.dto.user.AuthenticationDTO;
 import com.hoangjunss.junsBank.dto.user.UserCreateDTO;
 import com.hoangjunss.junsBank.dto.user.UserDTO;
+import com.hoangjunss.junsBank.entity.user.Account;
 import com.hoangjunss.junsBank.entity.user.User;
+import com.hoangjunss.junsBank.mapper.AccountMapper;
 import com.hoangjunss.junsBank.mapper.UserMapper;
 import com.hoangjunss.junsBank.producer.UserProducer;
+import com.hoangjunss.junsBank.service.Redis.RedisService;
+import com.hoangjunss.junsBank.service.User.AccountService;
 import com.hoangjunss.junsBank.service.User.UserService;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
 
 @Service
 public class UserApplicationService {
 
     @Autowired
-    private UserMapper userMapper; // Mapper để chuyển từ DTO sang Entity
+    private UserMapper userMapper;
+    @Autowired
+    private AccountMapper accountMapper;
+    @Autowired
+    private AccountService accountService;
+    @Autowired
+    private JwtTokenUtil jwtTokenUtil;
 
     @Autowired
-    private UserService userService; // Service để lưu vào cơ sở dữ liệu
+    private UserService userService;
+    @Autowired
+    private RedisService redisService;
 
     @Autowired
-    private UserProducer userProducer; // Producer để gửi sự kiện Kafka
-
+    private UserProducer userProducer;
     public void createUser(UserCreateDTO userCreateDTO) {
         // Chuyển đổi từ DTO sang Entity
         User user = userMapper.createToEntity(userCreateDTO);
@@ -39,6 +58,34 @@ public class UserApplicationService {
         userProducer.sendCreateUserEvent(user.getIdentificationNumber());
 
         return userMapper.toDTO(user);
+    }
+    @Transactional
+    public AuthenticationDTO login (AccountDTO accountDTO){
+        Account account=accountMapper.createToEntity(accountDTO);
+        Account accountLogin=accountService.login(account);
+        UserDetails userDetails=accountLogin.getUser();
+
+        String jwtToken=jwtTokenUtil.generateToken(userDetails);
+        Date expirationDate = jwtTokenUtil.getExpirationDate(jwtToken);
+        String expirationDateString = expirationDate.toString(); // Chuyển thành chuỗi
+
+        String refreshToken=jwtTokenUtil.generateRefreshToken(userDetails);
+        String tokenKey = RedisContants.WHITE_LIST_PASSWORD + jwtToken;
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        try {
+            // Chuyển `User` thành JSON
+            String userJson = objectMapper.writeValueAsString(userDetails);
+            long ttl = (expirationDate.getTime() - System.currentTimeMillis()) / 1000; // Tính TTL theo giây
+            // Lưu JSON `User` vào Redis với TTL
+            redisService.set(tokenKey, userJson);
+            redisService.setTimeToLive(tokenKey, ttl); // Đặt TTL phù hợp với thời gian sống của token
+        } catch (Exception e) {
+            // Xử lý lỗi nếu có
+            e.printStackTrace();
+            throw new RuntimeException("Failed to save user with token in Redis");
+        }
+        return AuthenticationDTO.builder().token(jwtToken).refreshToken(refreshToken).build();
     }
 
 }
--- junsBank/src/main/java/com/hoangjunss/junsBank/mapper/AccountMapper.java
+++ junsBank/src/main/java/com/hoangjunss/junsBank/mapper/AccountMapper.java
@@ -1,5 +1,6 @@
 package com.hoangjunss.junsBank.mapper;
 
+import com.hoangjunss.junsBank.dto.user.AccountDTO;
 import com.hoangjunss.junsBank.dto.user.UserCreateDTO;
 import com.hoangjunss.junsBank.entity.user.Account;
 import org.modelmapper.ModelMapper;
@@ -8,10 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 public class AccountMapper {
     @Autowired
     private ModelMapper modelMapper;
-    public Account createToEntity(UserCreateDTO userCreateDTO){
-        return Account.builder()
-                .password(userCreateDTO.getPassword())
-                .userCode(userCreateDTO.getPhoneNumber())
-                .build();
+    public Account createToEntity(AccountDTO accountDTO){
+        return modelMapper.map(accountDTO,Account.class);
     }
 }
